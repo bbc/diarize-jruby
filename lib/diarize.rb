01b513355f9d01042981e4139d6f72b1eae792aa
Yves Raimond <yves.raimond@bbc.co.uk>
2012-03-16 18:46:07 +0000

    Some success driving the first diarization from ruby

--- lib/diarize.rb
+++ lib/diarize.rb
@@ -1,1 +1,2 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'diarize', 'lium.rb')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'diarize', 'audio.rb')
---
+++ lib/diarize/audio.rb
@@ -0,0 +1,35 @@
+require 'tempfile'
+require File.join(File.expand_path(File.dirname(__FILE__)), 'lium')
+
+module Diarize
+
+  class Audio
+
+    def initialize(path)
+      @path = path
+    end
+
+    def diarize
+      seg_file = Tempfile.new(['diarize-jruby', '.seg'])
+      parameter = fr.lium.spkDiarization.parameter.Parameter.new
+      parameter.show = File.expand_path(@path).split('/')[-1].split('.')[0]
+      # 12 MFCC + Energy
+      # 1: static coefficients are present in the file
+      # 1: energy coefficient is present in the file
+      # 0: delta coefficients are not present in the file
+      # 0: delta energy coefficient is not present in the file
+      # 0: delta delta coefficients are not present in the file
+      # 0: delta delta energy coefficient is not present in the file
+      # 13: total size of a feature vector in the mfcc file
+      # 0:0:0: no feature normalization
+      parameter.parameterInputFeature.setFeaturesDescription('audio2sphinx,1:1:0:0:0:0,13,0:0:0:0')
+      parameter.parameterDiarization.cEClustering = true
+      parameter.parameterInputFeature.setFeatureMask(@path)
+      parameter.parameterSegmentationOutputFile.setMask(seg_file.path)
+      diarization = fr.lium.spkDiarization.system.Diarization.new
+      diarization.ester2Version(parameter)
+    end
+
+  end
+
+end
