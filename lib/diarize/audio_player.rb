8799fea34fc757c64b7f8aab7adec24dfe874762
Yves Raimond <yves.raimond@bbc.co.uk>
2012-03-19 17:13:03 +0000

    Adding a Javax.sampled-backed audio player

---
+++ lib/diarize/audio_player.rb
@@ -0,0 +1,25 @@
+require 'java'
+
+class Diarize
+
+  class AudioPlayer
+
+    def play(file, start=0.0, duration=10.0)
+      java_file = java.io.File.new(file.path)
+      stream = javax.sound.sampled.AudioSystem.getAudioInputStream(java_file)
+      clip = javax.sound.sampled.AudioSystem.clip
+      clip.open(stream)
+      clip.setMicrosecondPosition(duration * 1000000)
+      clip.start
+      begin
+        sleep(duration)
+      rescue Exception
+        $stderr.puts 'Stopping playback'
+      end
+      clip.stop
+      clip.close
+    end
+
+  end
+
+end
