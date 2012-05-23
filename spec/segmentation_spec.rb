d9eeb77efc24a69bc4cf497b9654c9ab0f484c66
Yves Raimond <yves.raimond@bbc.co.uk>
2012-05-23 14:57:29 +0100

    Fixing the specs

--- spec/segmentation_spec.rb
+++ spec/segmentation_spec.rb
@@ -3,7 +3,8 @@ require 'tempfile'
 
 describe Diarize::Segmentation, '#from_seg_file' do
 
-  audio = Diarize::Audio.new File.join(File.dirname(__FILE__), 'data', 'foo.wav')
+  audio_uri = URI('file:' + File.join(File.dirname(__FILE__), 'data', 'foo.wav'))
+  audio = Diarize::Audio.new audio_uri
 
   it "returns empty list from empty LIUM seg file" do
     seg_file = Tempfile.new(['diarize-jruby', '.seg'])
@@ -25,12 +26,12 @@ EOF
     segmentation.first.class.should eq(Diarize::Segment)
     segmentation.first.start.should eq(0)
     segmentation.first.duration.should eq(10)
-    segmentation.first.speaker.id.start_with?('S0').should be_true
+    segmentation.first.speaker.uri.should eq(URI(audio_uri.to_s + '#S0'))
     segmentation.first.speaker.gender.should eq('F')
     segmentation.last.class.should eq(Diarize::Segment)
     segmentation.last.start.should eq(10)
     segmentation.last.duration.should eq(100)
-    segmentation.last.speaker.id.start_with?('S1').should be_true
+    segmentation.last.speaker.uri.should eq(URI(audio_uri.to_s + '#S1'))
     segmentation.last.speaker.gender.should eq('M')
   end
 
