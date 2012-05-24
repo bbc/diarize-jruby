6b3a9965e2d3074ea6f15f2553c419f6fd382fed
Yves Raimond <yves.raimond@bbc.co.uk>
2012-05-24 17:28:54 +0100

    Changing message as a TODO

--- test/test_audio.rb
+++ test/test_audio.rb
@@ -41,7 +41,7 @@ class TestAudio < Test::Unit::TestCase
   end
 
   def test_analyze
-    # We don't test the full ESTER2 algorithm for now
+    # TODO - We don't test the full ESTER2 algorithm for now
   end
 
   def test_set_uri_and_type_uri
